05a86d7b90fdfbf6e4ce8f9d92009de2fcb6c62c
Louis-Philippe <louis-philippe.bigras@ivadolabs.com>
2024-01-19 16:14:39 -0500

    minor modf.

--- llm-assignment/main.py
+++ llm-assignment/main.py
@@ -7,20 +7,20 @@ load_dotenv()
 
 
 def list_jpeg_files(directory: str) -> list:
-    jpeg_files = []
+    result = []
     for filename in os.listdir(directory):
         if filename.lower().endswith((".jpeg", ".jpg")):
-            jpeg_files.append(os.path.join(directory, filename))
-    return jpeg_files
+            result.append(os.path.join(directory, filename))
+    return result
 
 
 if __name__ == "__main__":
     input_directory = "../data/ocr_dataset_original_size"
 
     jpeg_files = []
-    for filename in os.listdir(input_directory):
-        if filename.lower().endswith((".jpeg", ".jpg")):
-            jpeg_files.append(os.path.join(input_directory, filename))
+    for f in os.listdir(input_directory):
+        if f.lower().endswith((".jpeg", ".jpg")):
+            jpeg_files.append(os.path.join(input_directory, f))
 
     system_prompt = (
         "You are reading a form manually filled (Optical character recognition task), "
